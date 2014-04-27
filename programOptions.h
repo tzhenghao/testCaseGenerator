4759e3598a0825b92b680d452630e31a023107d1
Zheng Hao Tan <tanzhao@umich.edu>
2014-04-26 21:16:04 -0400

    Added programOptions.h that handles whitespaces and other functions

---
+++ programOptions.h
@@ -0,0 +1,29 @@
+// Zheng Hao Tan
+// Date: April 26, 2014
+
+// This file implements program options such as whitespaces in between the
+// numbers and words or special characters in between them.
+
+#ifndef PROGRAMOPTIONS_H
+#define PROGRAMOPTIONS_H
+
+#include <iostream>
+using namespace std;
+
+class programOptions {
+
+
+	public:
+
+		
+
+
+
+
+	private:
+	
+
+};
+
+
+
